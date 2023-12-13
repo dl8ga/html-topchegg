35b8d2dfa071944206afd0939e081a1624ed018f
dl8ga <152994862+dl8ga@users.noreply.github.com>
2023-12-13 10:02:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function func1(){
     let n=prompt('Введите почту:')
     alert('Форма заказа отправлена на почту: ' + n)
 }
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.querySelector(".ik").style.position = "fixed";
+    document.querySelector(".ik").style.display = "inline-block";
+  } else {
+    document.querySelector(".ik").style.position = "absolute";
+    document.querySelector(".ik").style.display = "none";
+  }
+}
